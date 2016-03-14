9b1c9b23050197091f56e9e31a2ea006dc5dd79a
谢振家 <xiezhenjia@xiezhenjiadeMac-mini.local>
2016-03-14 18:08:07 +0800

    design

--- design_test.go
+++ design_test.go
@@ -14,3 +14,10 @@ func TestInsert(*testing.T) {
 	//err := testRedis.INTO("task").FIELDS("name", "level", "count").VALUES("hyrra cache", 1, 2).INSERT()
 	fmt.Println(err)
 }
+
+//insert into user(id, name, age, pwd) values(1, 'xzj', 26, '123456')
+//select id, name, age, pwd from uer where ...
+//delete from user where ...
+//update user set age = 27 where name = 'xzj'
+
+//alter table user add index(index_name) (name)
